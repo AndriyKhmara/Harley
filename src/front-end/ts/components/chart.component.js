789d88ae4b377d1294cf02accdd91f0f09e8c2de
viktor <viktor.bobryk@gmail.com>
2017-03-23 08:27:47 +0200

    get data from selectors

--- src/front-end/ts/components/chart.component.js
+++ src/front-end/ts/components/chart.component.js
@@ -8,40 +8,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require('@angular/core');
 var ChartComponent = (function () {
     function ChartComponent() {
-        this.cars = [
-            { id: 1, name: 'a' },
-            { id: 2, name: 'b' },
-            { id: 3, name: 'c' }
-        ];
-        this.name = "Current Weather Chart";
-        this.className = "current-chart";
-        this.chartData = this.getChartData();
-        this.chartColors = this.getChartColors();
-        // this.cities = this.getCities();
-        // this.params = this.getParams();
-        this.chartLabels = this.getChartLabels();
-        this.chartOptions = this.getChartOptions();
         this.chartLegend = true;
-        this.chartType = "bar";
-    }
-    ChartComponent.prototype.getChartData = function () {
-        return [
-            { data: [2.2, 0, 0.27], label: 'darkSky' },
-            { data: [-1.37, -0.92, 1], label: 'openWeather' },
-            { data: [-1.68, 3.18, -2.3], label: 'wunderground' }
-        ];
-    };
-    ;
-    ChartComponent.prototype.getChartLabels = function () {
-        return ['Kiev', 'Rivne', 'Lutsk'];
-    };
-    ChartComponent.prototype.getChartOptions = function () {
-        return {
+        this.chartType = 'bar';
+        this.chartLabels = ['Kiev', 'Rivne', 'Lutsk'];
+        this.chartOptions = {
             responsive: true
         };
-    };
-    ChartComponent.prototype.getChartColors = function () {
-        return [
+        this.chartColors = [
             {
                 backgroundColor: '#ffbac7',
                 borderColor: '#ff1744',
@@ -67,38 +40,49 @@ var ChartComponent = (function () {
                 pointHoverBorderColor: '#2196f3'
             }
         ];
-    };
-    // public getCities() {
-    //     return  [
-    //         {id: 1, name: "Kiev"},
-    //         {id: 2, name: "Rivne"},
-    //         {id: 3, name: "Lutsk"}
-    //     ];
-    //
-    // }
-    // public getParams(){
-    //     return [
-    //         {id: 1, name: "wind speed"},
-    //         {id: 2, name: "temperature"},
-    //         {id: 3, name: "humidity"}
-    //     ];
-    // }
-    // ngOnInit () {
-    //     this.myForm = this.fb.group({
-    //         id: ['', Validators.required]
-    //     });
-    // }
-    ChartComponent.prototype.ngOnChanges = function () {
-        console.log("city:", this.cars);
+        this.cities = [
+            { id: 1, name: "Kiev" },
+            { id: 2, name: "Rivne" },
+            { id: 3, name: "Lutsk" }
+        ];
+        this.selectedCity = this.cities[1];
+        this.params = [
+            { id: 1, name: "wind speed" },
+            { id: 2, name: "temperature" },
+            { id: 3, name: "humidity" }
+        ];
+        this.selectedParam = this.params[1];
+        this.name = "Current Weather Chart";
+        this.className = "current-chart";
+        this.chartData = this.getChartData();
+    }
+    ChartComponent.prototype.getChartData = function () {
+        return [
+            { data: [2.2, 0, 0.27], label: 'darkSky' },
+            { data: [-1.37, -0.92, 1], label: 'openWeather' },
+            { data: [-1.68, 4.18, -2.3], label: 'wunderground' }
+        ];
     };
     ;
+    ChartComponent.prototype.onChangeCity = function (newCity) {
+        console.log("city:", newCity.name);
+        this.selectedCity = newCity;
+    };
+    ChartComponent.prototype.onChangeParam = function (newParam) {
+        console.log("param:", newParam.name);
+        this.selectedCity = newParam;
+    };
+    ChartComponent.prototype.ngOnChanges = function () {
+        console.log("city:", this.selectedCity.name);
+        console.log("city:", this.selectedCity.name);
+    };
     __decorate([
         core_1.Input()
     ], ChartComponent.prototype, "weather");
     ChartComponent = __decorate([
         core_1.Component({
             selector: 'chart',
-            template: "\n    <div class=\"{{className}} container\" style=\"width: 70%; height:auto; max-width: 600px border: 2px solid black\">\n        <div class=\"row\">\n            <div class=\"col-xs-12\">\n                <h2>{{name}}</h2>\n                <p>Temperature:</p>\n                <ul *ngIf=\"weather[0]\">\n                    <li *ngFor=\"let data of weather\">\n                        in {{data.cityName}} by {{data.sourceAPI}} is {{data.temp}}; \n                    </li>\n                </ul>\n                <!--<div>-->\n                    <!--<label>select city</label>-->\n                    <!--<select [( ngModel)]=\"selectedCity\">-->\n                        <!--<option *ngFor=\"let city of cities\" [ngValue]=\"city\">{{city.name}}</option>-->\n                    <!--</select>-->\n                    <!--<label>select params</label>-->\n                    <!--<select [(ngModel)]=\"selectedParam\">-->\n                        <!--<option *ngFor=\"let param of params\" [ngValue]=\"param\">{{param.name}}</option>-->\n                    <!--</select>-->\n                <!--</div>-->\n                <select name =\"id\" class = \"form-control\">\n                   <option *ngFor = \"let car of cars\" [ngValue] = \"car.key\">{{ car.name }}</option>\n                </select>\n                <canvas baseChart width=\"400\" height=\"400\"\n                [datasets]=\"chartData\"\n                [labels]=\"chartLabels\"\n                [options]=\"chartOptions\"\n                [colors]=\"chartColors\"\n                [legend]=\"chartLegend\"\n                [chartType]=\"chartType\"></canvas>\n            </div>\n        </div>\n    </div>"
+            template: "\n    <div class=\"{{className}} container\">\n        <div class=\"row\">\n            <div class=\"col-xs-12\">\n                <h2>{{name}}</h2>\n                <p>Temperature:</p>\n                <ul *ngIf=\"weather[0]\">\n                    <li *ngFor=\"let data of weather\">\n                        in {{data.cityName}} by {{data.sourceAPI}} is {{data.temp}}; \n                    </li>\n                </ul>\n                <div>\n                <label>select city</label>\n                <select [(ngModel)]=\"selectedCity\" (ngModelChange)=\"onChangeCity($event)\">\n                    <option *ngFor=\"let city of cities\" [ngValue]=\"city\">{{city.name}}</option>\n                </select>\n                <!--{{selectedCity | json}}-->\n                <label>select params</label>\n                <select [(ngModel)]=\"selectedParam\" (ngModelChange)=\"onChangeParam($event)\">\n                    <option *ngFor=\"let param of params\" [ngValue]=\"param\">{{param.name}}</option>\n                </select>\n                </div>\n                <canvas baseChart width=\"400\" height=\"400\"\n                [datasets]=\"chartData\"\n                [labels]=\"chartLabels\"\n                [options]=\"chartOptions\"\n                [colors]=\"chartColors\"\n                [legend]=\"chartLegend\"\n                [chartType]=\"chartType\"></canvas>\n            </div>\n        </div>\n    </div>"
         })
     ], ChartComponent);
     return ChartComponent;
--- src/front-end/ts/components/chart.component.ts
+++ src/front-end/ts/components/chart.component.ts
@@ -3,7 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
     selector: 'chart',
     template: `
-    <div class="{{className}} container" style="width: 70%; height:auto; max-width: 600px border: 2px solid black">
+    <div class="{{className}} container">
         <div class="row">
             <div class="col-xs-12">
                 <h2>{{name}}</h2>
@@ -13,19 +13,17 @@ import {Component, Input} from '@angular/core';
                         in {{data.cityName}} by {{data.sourceAPI}} is {{data.temp}}; 
                     </li>
                 </ul>
-                <!--<div>-->
-                    <!--<label>select city</label>-->
-                    <!--<select [( ngModel)]="selectedCity">-->
-                        <!--<option *ngFor="let city of cities" [ngValue]="city">{{city.name}}</option>-->
-                    <!--</select>-->
-                    <!--<label>select params</label>-->
-                    <!--<select [(ngModel)]="selectedParam">-->
-                        <!--<option *ngFor="let param of params" [ngValue]="param">{{param.name}}</option>-->
-                    <!--</select>-->
-                <!--</div>-->
-                <select name ="id" class = "form-control">
-                   <option *ngFor = "let car of cars" [ngValue] = "car.key">{{ car.name }}</option>
+                <div>
+                <label>select city</label>
+                <select [(ngModel)]="selectedCity" (ngModelChange)="onChangeCity($event)">
+                    <option *ngFor="let city of cities" [ngValue]="city">{{city.name}}</option>
                 </select>
+                <!--{{selectedCity | json}}-->
+                <label>select params</label>
+                <select [(ngModel)]="selectedParam" (ngModelChange)="onChangeParam($event)">
+                    <option *ngFor="let param of params" [ngValue]="param">{{param.name}}</option>
+                </select>
+                </div>
                 <canvas baseChart width="400" height="400"
                 [datasets]="chartData"
                 [labels]="chartLabels"
@@ -42,102 +40,78 @@ export class ChartComponent {
     public name: string;
     public className: string;
     public chartData: any;
-    public chartLabels: any;
-    public chartOptions: any;
-    public chartColors: any;
-    public cities: any;
-    public params: any;
-    public selectedCity:any;
-    public selectedParam:any;
-    public chartLegend:boolean;
-    public chartType:string;
+    public chartLegend:boolean = true;
+    public chartType:string = 'bar';
     @Input()
     public weather: any;
     public getChartData() {
         return [
             {data: [2.2, 0, 0.27 ], label: 'darkSky'},
             {data: [-1.37, -0.92, 1], label: 'openWeather'},
-            {data: [-1.68, 3.18, -2.3], label: 'wunderground'}
+            {data: [-1.68, 4.18, -2.3], label: 'wunderground'}
         ];
     };
-    public cars = [
-        {id: 1, name: 'a'},
-        {id: 2, name: 'b'},
-        {id: 3, name: 'c'}
+    public chartLabels:Array<any> = ['Kiev', 'Rivne', 'Lutsk'];
+    public chartOptions:any = {
+        responsive: true
+    };
+    public chartColors:Array<any> = [
+        { // pink
+            backgroundColor: '#ffbac7',
+            borderColor: '#ff1744',
+            pointBackgroundColor: '#ff1744',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: '#ff1744'
+        },
+        { // green
+            backgroundColor: '#e1f0d1',
+            borderColor: '#9ccc65',
+            pointBackgroundColor: '#9ccc65',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: '#9ccc65'
+        },
+        { // blue
+            backgroundColor: '#bde0fb',
+            borderColor: '#2196f3',
+            pointBackgroundColor: '#2196f3',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: '#2196f3'
+        }
     ];
 
-    public getChartLabels() {
-        return ['Kiev', 'Rivne', 'Lutsk'];
-    }
-    public getChartOptions() {
-        return {
-            responsive: true
-        };
-    }
-    public getChartColors() {
-        return [
-            { // pink
-                backgroundColor: '#ffbac7',
-                borderColor: '#ff1744',
-                pointBackgroundColor: '#ff1744',
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: '#fff',
-                pointHoverBorderColor: '#ff1744'
-            },
-            { // green
-                backgroundColor: '#e1f0d1',
-                borderColor: '#9ccc65',
-                pointBackgroundColor: '#9ccc65',
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: '#fff',
-                pointHoverBorderColor: '#9ccc65'
-            },
-            { // blue
-                backgroundColor: '#bde0fb',
-                borderColor: '#2196f3',
-                pointBackgroundColor: '#2196f3',
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: '#fff',
-                pointHoverBorderColor: '#2196f3'
-            }
-        ];
+    public cities = [
+        {id: 1, name: "Kiev"},
+        {id: 2, name: "Rivne"},
+        {id: 3, name: "Lutsk"}
+    ];
+    selectedCity = this.cities[1];
+    onChangeCity(newCity) {
+        console.log("city:", newCity.name);
+        this.selectedCity = newCity;
     }
 
-    // public getCities() {
-    //     return  [
-    //         {id: 1, name: "Kiev"},
-    //         {id: 2, name: "Rivne"},
-    //         {id: 3, name: "Lutsk"}
-    //     ];
-    //
-    // }
-    // public getParams(){
-    //     return [
-    //         {id: 1, name: "wind speed"},
-    //         {id: 2, name: "temperature"},
-    //         {id: 3, name: "humidity"}
-    //     ];
-    // }
-    // ngOnInit () {
-    //     this.myForm = this.fb.group({
-    //         id: ['', Validators.required]
-    //     });
-    // }
-    ngOnChanges(){
-        console.log ("city:", this.cars)
+    public params = [
+        {id: 1, name: "wind speed"},
+        {id: 2, name: "temperature"},
+        {id: 3, name: "humidity"}
+    ];
+    selectedParam = this.params[1];
+    onChangeParam(newParam) {
+        console.log("param:", newParam.name);
+        this.selectedCity = newParam;
     }
 
-    constructor(){
+    ngOnChanges() {
+        console.log ("city:", this.selectedCity.name);
+        console.log ("city:", this.selectedCity.name);
+    }
+
+    constructor() {
         this.name = "Current Weather Chart";
         this.className = "current-chart";
         this.chartData = this.getChartData();
-        this.chartColors = this.getChartColors();
-        // this.cities = this.getCities();
-        // this.params = this.getParams();
-        this.chartLabels = this.getChartLabels();
-        this.chartOptions = this.getChartOptions();
-        this.chartLegend = true;
-        this.chartType = "bar";
-        };
     }
-
+}
