a05b4fd4db494f19262170862372636326c1cbdf
editperson <lidiavaschik@gmail.com>
2017-03-24 19:30:09 +0200

    updates

--- src/front-end/ts/components/map.component.ts
+++ src/front-end/ts/components/map.component.ts
@@ -4,7 +4,7 @@ import { MapService } from '../services/map.service';
 @Component({
     selector: 'map',
     template: `<div><div id="map"></div></div>`,
-    styles:['#map{width:65%; height:35vh; margin:0 auto; z-index:-11111;}']
+    styles:['#map{width:65%; height:35vh; margin:0 auto; z-index:200;}']
 
 })
 
@@ -199,6 +199,21 @@ export class MapComponent implements OnChanges {
     }
 
     ngOnChanges() {
-        console.log(this.weather);
+        console.log("WEATHER:", this.weather);
+        
+        //this.renderMarkers();
+    }
+
+    renderMarkers(){
+        // var markers = this.weather;
+        // markers.forEach(function (item, i, arr) {
+        //     if (item.sourceAPI === "darkSky") {
+        //         let message = '<h4>'+ item.cityName + '</h4><ul><li>Temperature: <b>' + item.temp + '&deg;C</b></li><li>Pressure: <b>' + item.pressure +
+        //                    + ' mm Hg</b></li><li>Humidity: <b>' + item.humidity + ' %</b></li><li>Wind speed: <b>' + item.windSpeed + ' meter/sec</b></li></ul>'
+        //         L.marker([item.coords.lat, item.coords.lon]).addTo(map)
+        //       .bindPopup(message)
+        //       .openPopup();
+        //     }
+        // });
     }
 }
