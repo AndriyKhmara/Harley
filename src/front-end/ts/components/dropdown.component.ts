9e0fc40638bd3507b091c4b2bad7e6d56bf5cf03
Andy Khmara <andykhmara@gmail.com>
2017-03-27 21:15:22 +0300

    merge branches

--- src/front-end/ts/components/dropdown.component.ts
+++ src/front-end/ts/components/dropdown.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
  
 @Component({
   selector: 'dropdown',
-  templateUrl: './dropdown.template.html'
+  template: `<div class="btn-group" dropdown>
+  <button id="single-button" type="button" class="btn btn-primary" dropdownToggle>
+    Button dropdown <span class="caret"></span>
+  </button>
+  <ul dropdownMenu role="menu" aria-labelledby="single-button">
+    <li role="menuitem"><a class="dropdown-item" href="#">Action</a></li>
+    
+  </ul>
+</div>`
 })
 export class DropdownComponent {
 }
